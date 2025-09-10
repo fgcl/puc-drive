f14971b755335e9c6c8d948baebf4590552f3712
Felipe Gonçalves <lipegcl@gmail.com>
2025-09-10 10:41:19 -0300

    rotacionamento

--- segundo/labmod/20250820-aula1/teste/main.cpp
+++ segundo/labmod/20250820-aula1/teste/main.cpp
@@ -3,62 +3,113 @@
 @note Desenha linha horizontal
 @author Felipe Gonçalves
 */
-#ifdef __APPLE__
-#include <GLUT/glut.h>
-#else
+//#ifdef __APPLE__
+//#include <GLUT/glut.h>
+//#else
 #include <GL/glut.h>
-#endif
+//#endif
 #include <stdlib.h>
 #include <stdio.h>
 
-void desenha(void){
+GLfloat escala = 1;
+GLfloat posx = 0, posy = 0, angle = 0;
+
+void desenha(void)
+{
     glClear(GL_COLOR_BUFFER_BIT);
     // gluOrtho(x,xf,y,yf) //começo
 
     // cores
     glColor3f(1.0f,1.0f,0.0f); // linha amarela
 
+    glMatrixMode(GL_PROJECTION); // define a matrix da camera
+    glLoadIdentity();
+    gluOrtho2D(-10, 10, -10, 10); // escala inicial da matrix
+
+    glTranslatef(posx, posy, 0);
+    glScalef(escala, escala, 0);
+    glRotatef(angle, 0, 0, 1);
+
+    glMatrixMode(GL_MODELVIEW);// matrix dos objetos
+    glLoadIdentity();
+
     // formas
+
     // F
     glBegin(GL_LINE_LOOP);
-        glVertex2f(-0.9f, -0.9f);
-        glVertex2f(-0.9f, 0.5f);
-        glVertex2f(-0.6f, 0.5f);
-        glVertex2f(-0.6f, 0.4f);
-        glVertex2f(-0.8f, 0.4f);
-        glVertex2f(-0.8f, 0.0f);
-        glVertex2f(-0.6f, 0.0f);
-        glVertex2f(-0.6f, -0.1f);
-        glVertex2f(-0.8f, -0.1f);
-        glVertex2f(-0.8f, -0.9f);
+    glVertex2f(-9, -9); // ponto a
+    glVertex2f(-9, 9); //ponto b
+    glVertex2f(-5, 9); // ponto c
+    glVertex2f(-5, 7); // ponto d
+    glVertex2f(-7, 7); // ponto e
+    glVertex2f(-7, 1); // ponto f
+    glVertex2f(-5, 1); //ponto g
+    glVertex2f(-5, -1); // ponto h
+    glVertex2f(-7, -1); // ponto i
+    glVertex2f(-7, -9); // ponto j
     glEnd();
 
     // E
     glBegin(GL_LINE_LOOP);
-        glVertex2f(-0.5f, -0.9f);
-        glVertex2f(-0.5f, 0.5f);
-        glVertex2f(-0.2f, 0.5f);
-        glVertex2f(-0.2f, 0.4f);
-        glVertex2f(-0.4f, 0.4f);
-        glVertex2f(-0.4f, 0.0f);
-        glVertex2f(-0.2f, 0.0f);
-        glVertex2f(-0.2f, -0.1f);
-        glVertex2f(-0.4f, -0.1f);
-        glVertex2f(-0.4f, -0.8f);
-        glVertex2f(-0.2f, -0.8f);
-        glVertex2f(-0.2f, -0.9f);
+    glVertex2f(-3, -9); // a
+    glVertex2f(-3, 9); // b
+    glVertex2f(3, 9); // c
+    glVertex2f(3, 7); // d
+    glVertex2f(-1, 7); // e
+    glVertex2f(-1, 1); // f
+    glVertex2f(3, 1); // g
+    glVertex2f(3, -1); // h
+    glVertex2f(-1, -1); // i
+    glVertex2f(-1, -7); // j
+    glVertex2f(3, -7); // k
+    glVertex2f(3, -9); // l
     glEnd();
 
     // L
     glBegin(GL_LINE_LOOP);
-        glVertex2f(-0.1f, -0.9f);
-        glVertex2f(-0.1f, 0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.0f, -0.8f);
-        glVertex2f(0.2f, -0.8f);
-        glVertex2f(0.2f, -0.9f);
+    glVertex2f(5, -9); // a
+    glVertex2f(5, 9); // b
+    glVertex2f(7, 9); // c
+    glVertex2f(7, -7); // d
+    glVertex2f(9, -7); // e
+    glVertex2f(9, -9); //f
     glEnd();
     glFlush();
+}
+
+void listeningKey(unsigned char tecla, GLint x, GLint y)
+{
+    if(escala <= 1.0){
+        escala = 1.0;
+    }
+    switch(tecla)
+    {
+    case 'm':
+        escala++;
+        break;
+    case 'n':
+        escala--;
+        break;
+    case 'a':
+        posx++;
+        break;
+    case 'd':
+        posx--;
+        break;
+    case 'w':
+        posy++;
+        break;
+    case 's':
+        posy--;
+        break;
+    case 'q':
+        angle++;
+        break;
+    case 'e':
+        angle--;
+        break;
+    }
+    desenha();
 }
 
 int main(int argc, char *argv[])
@@ -71,6 +122,9 @@ int main(int argc, char *argv[])
     glutInitWindowPosition(0,0); // posicao da janela
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // GLUT_DOUBLE = animaçao
     glutCreateWindow("Meus graficos");
+
+    glutKeyboardFunc(listeningKey); // para ler o teclado
+
     glutDisplayFunc(desenha);
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
